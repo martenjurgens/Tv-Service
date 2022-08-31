ebbf483ff2cedc1d3215ecd04134f1e13ef876aa
Marten Jürgens <55079581+martenjurgens@users.noreply.github.com>
2022-08-31 03:58:38 +0300

    Removed unused stuff

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,10 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import OrderList from './components/OrderList'
 import Pricing from './components/Pricing'
-import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
@@ -21,8 +20,3 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
